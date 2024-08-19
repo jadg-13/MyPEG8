ab2d680dbaae4d48807ea79bb59190436610d0b3
Laboratorio C202 <labc202@uam.net.ni>
2024-08-19 10:46:11 -0600

    Sesion1

---
+++ HolaMundo/HolaMundo/Ejemplo1.cs
@@ -0,0 +1,34 @@
+﻿namespace HolaMundo
+{
+    internal class Ejemplo1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Gabriel Lacayo");
+
+            // Tipos de datos primitivos
+            int edad = 20;
+            Console.WriteLine($"Tu edad es: {edad}");
+
+            string nombre = "Gabriel Norwin Lacayo Marenco";
+            Console.WriteLine($"Tu nombre completo es: {nombre}");
+            Console.WriteLine($"Tu nombre completo es: {nombre.ToUpper()}");
+            Console.WriteLine($"Tu nombre completo es: {nombre.ToLower()}");
+            Console.WriteLine($"Tu nombre completo tiene: {nombre.Length} letras");
+
+            //Variabls con doble precision
+            Double precio = 39.90;
+            Console.WriteLine($"El precio es de: ${precio}");
+            Console.WriteLine($"El precio es de: ${precio++}");
+            Console.WriteLine($"El precio es de: ${++precio}");
+
+            /* Valores logicos*/
+            Boolean isSingle = true;
+            Console.WriteLine($"¿Tu eres soltero?: {isSingle}");
+            Console.WriteLine($"¿Tu eres soltero?: {!isSingle}");
+            Console.WriteLine($"¿Tu eres soltero?: {isSingle}");
+        }
+
+    }
+}
