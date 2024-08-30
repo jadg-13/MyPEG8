928cd24d3aaefdee154594742f23ce02b5b14d79
jadg13 <jadg13@hotmail.com>
2024-08-30 10:43:26 -0600

    fin de clase

--- Sesion4/Form1.Designer.cs
+++ Sesion4/Form1.Designer.cs
@@ -30,6 +30,9 @@
       {
          this.btnAumentar = new System.Windows.Forms.Button();
          this.btnDisminuir = new System.Windows.Forms.Button();
+         this.nudPorcentaje = new System.Windows.Forms.NumericUpDown();
+         this.progressBar1 = new System.Windows.Forms.ProgressBar();
+         ((System.ComponentModel.ISupportInitialize)(this.nudPorcentaje)).BeginInit();
          this.SuspendLayout();
          // 
          // btnAumentar
@@ -52,13 +55,31 @@
          this.btnDisminuir.UseVisualStyleBackColor = true;
          this.btnDisminuir.Click += new System.EventHandler(this.btnDisminuir_Click);
          // 
+         // nudPorcentaje
+         // 
+         this.nudPorcentaje.Location = new System.Drawing.Point(239, 85);
+         this.nudPorcentaje.Name = "nudPorcentaje";
+         this.nudPorcentaje.Size = new System.Drawing.Size(120, 35);
+         this.nudPorcentaje.TabIndex = 2;
+         this.nudPorcentaje.ValueChanged += new System.EventHandler(this.nudPorcentaje_ValueChanged);
+         // 
+         // progressBar1
+         // 
+         this.progressBar1.Location = new System.Drawing.Point(239, 153);
+         this.progressBar1.Name = "progressBar1";
+         this.progressBar1.Size = new System.Drawing.Size(238, 23);
+         this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+         this.progressBar1.TabIndex = 3;
+         // 
          // Form1
          // 
          this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 29F);
          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
          this.AutoScroll = true;
          this.BackColor = System.Drawing.SystemColors.Highlight;
-         this.ClientSize = new System.Drawing.Size(614, 749);
+         this.ClientSize = new System.Drawing.Size(614, 268);
+         this.Controls.Add(this.progressBar1);
+         this.Controls.Add(this.nudPorcentaje);
          this.Controls.Add(this.btnDisminuir);
          this.Controls.Add(this.btnAumentar);
          this.Font = new System.Drawing.Font("Pristina", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -70,6 +91,8 @@
          this.Opacity = 0.75D;
          this.ShowIcon = false;
          this.Text = "Ejemplo1";
+         this.Load += new System.EventHandler(this.Form1_Load);
+         ((System.ComponentModel.ISupportInitialize)(this.nudPorcentaje)).EndInit();
          this.ResumeLayout(false);
 
       }
@@ -78,6 +101,8 @@
 
       private System.Windows.Forms.Button btnAumentar;
       private System.Windows.Forms.Button btnDisminuir;
+      private System.Windows.Forms.NumericUpDown nudPorcentaje;
+      private System.Windows.Forms.ProgressBar progressBar1;
    }
 }
 
--- Sesion4/Form1.cs
+++ Sesion4/Form1.cs
@@ -12,29 +12,36 @@ namespace Sesion4
 {
    public partial class Form1 : Form
    {
+      public int Porcentaje { get; set; }
+
       public Form1()
       {
          InitializeComponent();
+         Porcentaje = (int)(this.Opacity * 100);
+         nudPorcentaje.Value = Porcentaje;
       }
 
       private void btnAumentar_Click(object sender, EventArgs e)
       {
-
-         this.Opacity = (this.Opacity>= 1)?1: this.Opacity+= 0.01;
+         this.Opacity = (this.Opacity >= 1) ? 1 : this.Opacity += 0.01;
          mostrarOpacity();
+         this.nudPorcentaje.Value = (int)(this.Opacity * 100);
       }
 
       private void btnDisminuir_Click(object sender, EventArgs e)
       {
-         this.Opacity = (this.Opacity<=0.2)?0.2:  this.Opacity-= 0.01;
+         this.Opacity = (this.Opacity <= 0.2) ? 0.2 : this.Opacity -= 0.01;
          mostrarOpacity();
+         this.nudPorcentaje.Value = (int)(this.Opacity * 100);
       }
 
       private void mostrarOpacity()
       {
          this.Text = "";
-         this.Text = "Ejemplo 1 - " + (this.Opacity * 100).ToString() + "%";
-         if(this.Opacity < 0.5) {
+         this.Text = "Ejemplo 1 - " + (this.Opacity * 100) + "%";
+
+         if (this.Opacity < 0.5)
+         {
             this.BackColor = Color.Red;
          }
          else
@@ -43,6 +50,17 @@ namespace Sesion4
          }
       }
 
-    
+      private void Form1_Load(object sender, EventArgs e)
+      {
+
+      }
+
+      private void nudPorcentaje_ValueChanged(object sender, EventArgs e)
+      {
+         this.Opacity=  (double) (nudPorcentaje.Value/100);
+         mostrarOpacity();
+         progressBar1.Value = (int) nudPorcentaje.Value;
+
+      }
    }
 }
